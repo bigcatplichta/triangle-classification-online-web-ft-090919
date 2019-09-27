d642ceaa011f3689135e66ff223f964eb9055ab8
Matt Plichta <bigcatplichta@gmail.com>
2019-09-27 05:43:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -29,7 +29,9 @@ class Triangle
   end 
   
   def illegal?
-    (@side1 == 0 || @side2 == 0 || @side3 == 0) || (@side1 > (@side2 + @side3))
+    if @side1 == 0 || @side2 == 0 || @side3 == 0
+      true 
+    elsif @side1 > (@side2 + @side3) || @side2 > (@side1 + @side3) || @side3 > (@side1 + @side2)
   end 
   
   class TriangleError < StandardError
