d30baad6cd0bbe7c996b82eae8ba4a65a92eff5c
Matt Plichta <bigcatplichta@gmail.com>
2019-09-27 05:24:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -29,7 +29,7 @@ class Triangle
   end 
   
   def illegal?
-    (@side1 == 0 or @side2 == 0 or @side3 == 0) || (@side1 > (@side2 + @side3))
+    (@side1 == 0 || @side2 == 0 || @side3 == 0) || (@side1 > (@side2 + @side3))
     
   end 
   
